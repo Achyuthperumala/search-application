b4921aa3b0e7dbc8e9f5b9de306cf0e712087445
Achyuth Perumala <101448384+Achyuthperumala@users.noreply.github.com>
2024-02-23 14:26:55 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import SearchForm from './SearchForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Search and Display</h1>
+      <SearchForm />
+    </div>
+  );
+}
+
+export default App;
